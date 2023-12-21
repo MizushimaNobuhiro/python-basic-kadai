e29b7893bb3981d319ca244f67183125bcf69b90
Mizushima_Nobuhiro <149216813+MizushimaNobuhiro@users.noreply.github.com>
2023-12-21 13:23:47 +0900

    再々提出_kadai_013

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,6 +1,5 @@
 def commodity_total(price, tax):
-  tax = price * 0.1
-  commodity_total = price + tax
+  commodity_total = price * tax
   print(f"{price}円(税込：{commodity_total}円)")
   
-commodity_total(800, 10)
+commodity_total(800, 1.1)
