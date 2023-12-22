380ed74d573cd37dbe9823396fecb324a5e801b2
Mizushima_Nobuhiro <149216813+MizushimaNobuhiro@users.noreply.github.com>
2023-12-22 15:50:36 +0900

    再提出_kadai_015

--- kadai_015/kadai_015.py
+++ kadai_015/kadai_015.py
@@ -2,8 +2,16 @@ class Human:
   def __init__(self, name, age):
     self.name = name
     self.age = age
+    
+  def set_name(self, name):
+    self.name = name
+  def set_age(self, age):
+    self.age = age
+    
+  def printinfo(self):
+    print("名前:", self.name)
+    print("年齢:", self.age)
 
-printinfo = Human("名前", "年齢")
+human = Human("侍太郎", 18)
 
-print(printinfo.name)
-print(printinfo.age)
+human.printinfo()
